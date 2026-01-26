b16a0b44fa0fa0713500087e3aaa5105976a7bf0
Omar Saied <omar.gooda02@gmail.com>
2026-01-26 15:56:52 +0200

    ......

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const jsonServer = require('json-server');
+const path = require('path');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json'); // Path to your db.json file
-const middlewares = jsonServer.defaults();
+const middlewares = jsonServer.defaults({
+  static: path.join(__dirname, 'public')
+});
 
 server.use(middlewares);
 server.use(router);
